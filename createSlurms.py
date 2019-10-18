85800a2f95223c8ca979a9844e148de4b61356eb
frtim <frtim@ethz.ch>
2019-10-18 13:06:11 -0400

    edited slurm file, ran without errors on cluster

--- createSlurms.py
+++ createSlurms.py
@@ -1,20 +1,20 @@
 import os
 import param
-from functions import makeFolder
 
 template = '''#!/bin/bash
 #
 # add all other SBATCH directives here
 #
-#SBATCH -p serial_requeue                                                                                                                           # use the COX partition
-#SBATCH -n 1                                                                                                                                        # Number of cores
-#SBATCH -N 1                                                                                                                                        # Ensure that all cores are on one matching
-#SBATCH --mem=10000                                                                                                                                 # CPU memory in GBs
-#SBATCH -t 0-00:10                                                                                                                                  # time in dd-hh:mm to run the code for
-#SBATCH --mail-type=NONE                                                                                                                             # send all email types (start, end, error, etc.)
-#SBATCH --mail-user=tfranzmeyer@g.harvard.edu                                                                                                       # email address to send to
-#SBATCH -o {ERROR_PATH}/{JOBNAME}.out       # where to write the log files
-#SBATCH -e {OUTPUT_PATH}/{JOBNAME}.err        # where to write the error files
+#SBATCH -p holyseasgpu                                       # use the COX partition
+#SBATCH -n 1                                                 # Number of cores
+#SBATCH -N 1                                                 # Ensure that all cores are on one matching
+#SBATCH --mem=10000                                          # CPU memory in GBs
+#SBATCH -t 0-00:10                                           # time in dd-hh:mm to run the code for
+#SBATCH --mail-type=NONE                                     # send all email types (start, end, error, etc.)
+#SBATCH --mail-user=tfranzmeyer@g.harvard.edu                # email address to send to
+#SBATCH -o {OUTPUT_PATH}/{JOBNAME}.out                       # where to write the log files
+#SBATCH -e {ERROR_PATH}/{JOBNAME}.err                        # where to write the error files
+#SBATCH -J fillholes_{JOBNAME}                               # jobname given to job 
 
 module load Anaconda3/5.0.1-fasrc02
 module load cuda/9.0-fasrc02 cudnn/7.1_cuda9.0-fasrc01
@@ -30,6 +30,14 @@ python {COMMAND}
 echo "DONE"
 
 '''
+
+
+def makeFolder(folder_path):
+    if os.path.exists(folder_path):
+        raise ValueError("Folderpath " + folder_path + " already exists!")
+    else:
+        os.mkdir(folder_path)
+
 
 def writeFile(filename, data):
     if os.path.exists(filename):
