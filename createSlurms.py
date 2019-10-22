008438798acc01da0393bf10729f16db727c8fb3
fratim <frtim@student.ethz.ch>
2019-10-22 18:37:10 -0400

    changed createslurms to 3 steps os stepTwo (A,B,C)

--- createSlurms.py
+++ createSlurms.py
@@ -5,12 +5,12 @@ template = '''#!/bin/bash
 #
 # add all other SBATCH directives here
 #
-#SBATCH -p shared                                      # use the COX partition
+#SBATCH -p shared                                            # use the COX partition
 #SBATCH -n 1                                                 # Number of cores
 #SBATCH -N 1                                                 # Ensure that all cores are on one matching
 #SBATCH --mem={MEMORY}                                       # CPU memory in MBs
 #SBATCH -t 0-01:00                                           # time in dd-hh:mm to run the code for
-#SBATCH --mail-type=NONE                                      # send all email types (start, end, error, etc.)
+#SBATCH --mail-type=NONE                                     # send all email types (start, end, error, etc.)
 #SBATCH --mail-user=tfranzmeyer@g.harvard.edu                # email address to send to
 #SBATCH -o {OUTPUT_PATH}/{JOBNAME}.out                       # where to write the log files
 #SBATCH -e {ERROR_PATH}/{JOBNAME}.err                        # where to write the error files
@@ -54,14 +54,18 @@ SLURM_OUTPUT_FOLDER = '/n/home12/tfranzmeyer/slurm_files/'
 
 step00folderpath = SLURM_OUTPUT_FOLDER+"step00/"
 step01folderpath = SLURM_OUTPUT_FOLDER+"step01/"
-step02folderpath = SLURM_OUTPUT_FOLDER+"step02/"
+step02Afolderpath = SLURM_OUTPUT_FOLDER+"step02A/"
+step02Bfolderpath = SLURM_OUTPUT_FOLDER+"step02B/"
+step02Bfolderpath = SLURM_OUTPUT_FOLDER+"step02C/"
 step03folderpath = SLURM_OUTPUT_FOLDER+"step03/"
 step04folderpath = SLURM_OUTPUT_FOLDER+"step04/"
 
 makeFolder(step00folderpath)
 print(step00folderpath)
 makeFolder(step01folderpath)
-makeFolder(step02folderpath)
+makeFolder(step02Afolderpath)
+makeFolder(step02Bfolderpath)
+makeFolder(step02Cfolderpath)
 makeFolder(step03folderpath)
 makeFolder(step04folderpath)
 
@@ -81,7 +85,6 @@ writeFile(filename, t)
 files_written += 1
 
 # write slurm for step one
-
 for bz in range(param.z_start, param.z_start + param.n_blocks_z):
     for by in range(param.y_start, param.y_start + param.n_blocks_y):
         for bx in range(param.x_start, param.x_start + param.n_blocks_x):
@@ -100,8 +103,44 @@ for bz in range(param.z_start, param.z_start + param.n_blocks_z):
             writeFile(filename, t)
             files_written += 1
 
-# Write Slurm for step two
-command = "stepTwo.py"
+# write slurm for step two A
+for bz in range(param.z_start, param.z_start + param.n_blocks_z):
+
+    command = "stepTwoA.py" + " " + str(bz)
+    jobname = "step02A"+"_"+param.outp_ID+"_" +"z"+str(bz).zfill(4)+"y"+str(by).zfill(4)+"x"+str(bx).zfill(4)
+
+    t = template
+    t = t.replace('{JOBNAME}', jobname)
+    t = t.replace('{COMMAND}', command)
+    t = t.replace('{ERROR_PATH}', param.error_path)
+    t = t.replace('{OUTPUT_PATH}', param.output_path)
+    t = t.replace('{MEMORY}', str(memory_std))
+
+    filename = step02Afolderpath + jobname + ".slurm"
+    writeFile(filename, t)
+    files_written += 1
+
+# write slurm for step two B
+for it in range(2, param.iterations_needed+1)
+    for bz in range(param.z_start, param.z_start + param.n_blocks_z):
+
+        command = "stepTwoB.py" + " " + str(bz) + " " + str(it)
+        jobname = "step02A"+"_"+param.outp_ID+"_" +"z"+str(bz).zfill(4)+"y"+str(by).zfill(4)+"x"+str(bx).zfill(4)
+
+        t = template
+        t = t.replace('{JOBNAME}', jobname)
+        t = t.replace('{COMMAND}', command)
+        t = t.replace('{ERROR_PATH}', param.error_path)
+        t = t.replace('{OUTPUT_PATH}', param.output_path)
+        t = t.replace('{MEMORY}', str(memory_std))
+
+        filename = step02Bfolderpath + jobname + ".slurm"
+        writeFile(filename, t)
+        files_written += 1
+
+
+# Write Slurm for step two C
+command = "stepTwoC.py"
 jobname = "step02"+"_"+param.outp_ID
 
 t = template
@@ -111,7 +150,7 @@ t = t.replace('{ERROR_PATH}', param.error_path)
 t = t.replace('{OUTPUT_PATH}', param.output_path)
 t = t.replace('{MEMORY}', str(memory_std))
 
-filename = step02folderpath + jobname + ".slurm"
+filename = step02Cfolderpath + jobname + ".slurm"
 writeFile(filename, t)
 files_written += 1
 
