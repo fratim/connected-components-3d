384bc1d3df5c5f8edf24bfc49c2f5d09fa242f48
fratim <frtim@student.ethz.ch>
2019-10-22 11:34:16 -0400

    changes, not working commit

--- fillHoles.bash
+++ fillHoles.bash
@@ -3,30 +3,30 @@ set -e
 
 echo "Execution started"
 
-# # prepare folders
-# python preparation.py
-#
-# # Execute step one
-# for bz in {2..17}
-# do
-#   for by in {0..1}
-#   do
-#     for bx in {0..1}
-#       do
-#         python stepOne.py $bz $by $bx
-#     done
-#   done
-# done
-#
-# echo "Step 1 finished"
-#
-# # # execute step 2 (find associatted labels_global)
-# python stepTwo.py
-#
-# echo "Step 2 finished"
+# prepare folders
+python preparation.py
+
+# Execute step one
+for bz in {2..18}
+do
+  for by in {0..1}
+  do
+    for bx in {0..1}
+      do
+        python stepOne.py $bz $by $bx
+    done
+  done
+done
+
+echo "Step 1 finished"
+
+# # execute step 2 (find associatted labels_global)
+python stepTwo.py
+
+echo "Step 2 finished"
 
 # execute step 3 (fill holes)
-for bz in {2..17}
+for bz in {2..18}
 do
   for by in {0..1}
   do
