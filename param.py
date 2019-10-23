74f71bd869df1e8a9b6975a404736b159677da88
frtim <frtim@ethz.ch>
2019-10-23 13:46:39 -0400

    code used to compute fullSize1to20

--- param.py
+++ param.py
@@ -4,35 +4,35 @@ import math
 isCluster = True
 
 if isCluster:
-    output_path = "/n/home12/tfranzmeyer/wiring/raw_data/segmentations/Zebrafinch/stacked_volumes/"
-    data_path = "/n/home12/tfranzmeyer/wiring/raw_data/segmentations/Zebrafinch/stacked_volumes/"
-    sample_name = "ZF_concat_2to15_0256_0256"
-
-    # compute number of blocks and block size
-    #bs_z = 128
-    #n_blocks_z = 20
-    #bs_y = 5456
-    #n_blocks_y = 1
-    #bs_x = 5332
-    #n_blocks_x = 1
-
-    # start slice of zebrafinch block
-    #z_start = 0
-    #y_start = 0
-    #x_start = 0
+    output_path = "/n/home12/tfranzmeyer/wiring/raw_data/segmentations/Zebrafinch/"
+    data_path = "/n/home12/tfranzmeyer/wiring/raw_data/segmentations/Zebrafinch/"
+    sample_name = ""
 
     # compute number of blocks and block size
     bs_z = 128
-    n_blocks_z = 14
-    bs_y = 128
-    n_blocks_y = 2
-    bs_x = 128
-    n_blocks_x = 2
+    n_blocks_z = 20
+    bs_y = 5456
+    n_blocks_y = 1
+    bs_x = 5332
+    n_blocks_x = 1
 
-    # this has to set here and in the bash script
-    z_start = 2
+    # start slice of zebrafinch block
+    z_start = 0
     y_start = 0
     x_start = 0
+
+    # compute number of blocks and block size
+    #bs_z = 128
+    #n_blocks_z = 14
+    #bs_y = 128
+    #n_blocks_y = 2
+    #bs_x = 128
+    #n_blocks_x = 2
+
+    # this has to set here and in the bash script
+    #z_start = 2
+    #y_start = 0
+    #x_start = 0
 
 if not isCluster:
     output_path = "/home/frtim/wiring/raw_data/segmentations/Zebrafinch/stacked_volumes/"
@@ -53,7 +53,7 @@ if not isCluster:
     y_start = 0
     x_start = 0
 
-outp_ID = "newBashFresh2"
+outp_ID = "fullSize_1to20"
 
 folder_path = data_path + sample_name + "/" + outp_ID + "/"
 
