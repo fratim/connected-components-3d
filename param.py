3fb03ebd62d812ce6d1f22c1f54c63b0b1f5de10
fratim <frtim@student.ethz.ch>
2019-10-29 14:26:47 -0400

    param update

--- param.py
+++ param.py
@@ -10,17 +10,17 @@ isCluster = True
 if isCluster:
     output_path = "/n/pfister_lab2/Lab/tfranzmeyer/Zebrafinch/"
     data_path = "/n/pfister_lab2/Lab/tfranzmeyer/Zebrafinch/"
-    sample_name = "labels_512_2048_2048/"
+    sample_name = ""
 
     # compute number of blocks and block size
-    max_bs_z = 512
-    n_blocks_z = 12
+    max_bs_z = 128
+    n_blocks_z = 20
 
-    max_bs_y = 2048
-    n_blocks_y = 3
+    max_bs_y = 5456
+    n_blocks_y = 1
 
-    max_bs_x = 2048
-    n_blocks_x = 3
+    max_bs_x = 5332
+    n_blocks_x = 1
 
     # start slice of zebrafinch block
     z_start = 0
@@ -61,7 +61,7 @@ if not isCluster:
     y_start = 0
     x_start = 0
 
-outp_ID = "AllFiles"
+outp_ID = "1to20New"
 
 folder_path = data_path + sample_name + "/" + outp_ID + "/"
 
@@ -81,6 +81,6 @@ step02B_info_filepath = folder_path+"step02B_info.txt"
 step03_info_filepath  = folder_path+"step03_info.txt"
 
 # memory need per block (in MB)
-memory_needed = 40000 #int(1.1*bs_z*bs_y*bs_x*(8+8+2)/1000/1000)
+memory_needed = 80000 #int(1.1*bs_z*bs_y*bs_x*(8+8+2)/1000/1000)
 
 max_labels_block = max_bs_z*max_bs_y*max_bs_x
