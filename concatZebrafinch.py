d3f0a7b3ee079ef36cf577a7926a12328aced83d
frtim <frtim@ethz.ch>
2019-10-30 08:27:02 -0400

    Code for stacked512 2048 2048

--- concatZebrafinch.py
+++ concatZebrafinch.py
@@ -22,21 +22,23 @@ from functions import readData, makeFolder, blockFolderPath, writeData
 zrange = np.arange(param.z_start,param.z_start+param.n_blocks_z)
 
 sample_name = "labels_512_2048_2048"
+folder_path = "/n/pfister_lab2/Lab/tfranzmeyer/Zebrafinch/"
 
 for z_block in range(12):
     for y_block in range(3):
-        filename = param.folder_path+"/"+sample_name+"/"+"/z"+str(z_block).zfill(2)+"y"+str(y_block).zfill(2)+"x"+str(0).zfill(2)
+        filename = folder_path+"/"+sample_name+"/"+"/z"+str(z_block).zfill(2)+"y"+str(y_block).zfill(2)+"x"+str(0).zfill(2)
         block_0 = readData(box=[1],filename=filename)
 
-        filename = param.folder_path+"/"+sample_name+"/"+"/z"+str(z_block).zfill(2)+"y"+str(y_block).zfill(2)+"x"+str(1).zfill(2)
+        filename = folder_path+"/"+sample_name+"/"+"/z"+str(z_block).zfill(2)+"y"+str(y_block).zfill(2)+"x"+str(1).zfill(2)
         block_1 = readData(box=[1],filename=filename)
 
-        filename = param.folder_path+"/"+sample_name+"/"+"/z"+str(z_block).zfill(2)+"y"+str(y_block).zfill(2)+"x"+str(2).zfill(2)
+        filename = folder_path+"/"+sample_name+"/"+"/z"+str(z_block).zfill(2)+"y"+str(y_block).zfill(2)+"x"+str(2).zfill(2)
         block_2 = readData(box=[1],filename=filename)
 
-        x_block = np.concatenate((block_0,block_1,block_2),axis=3)
+        x_block = np.concatenate((block_0,block_1,block_2),axis=2)
+	print(x_block.shape)
 
-        if y_block = 0:
+        if y_block == 0:
             y_block=x_block.copy()
         else:
             y_block = np.concatenate((y_block,x_block),axis=1)
@@ -46,5 +48,5 @@ for z_block in range(12):
     for i in range(4):
         chunk = y_block[i*128:((i+1)*128),:,:]
         print(chunk.shape)
-        writeData(param.folder_path+"/"+sample_name+"/"str(z_index).zfill(4),chunk)
+        writeData(folder_path+"/"+sample_name+"/"+str(z_index).zfill(4),chunk)
         z_index+=128
--- param.py
+++ param.py
@@ -61,7 +61,7 @@ if not isCluster:
     y_start = 0
     x_start = 0
 
-outp_ID = "1to20New"
+outp_ID = ""
 
 folder_path = data_path + sample_name + "/" + outp_ID + "/"
 
