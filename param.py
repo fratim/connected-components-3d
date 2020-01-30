efad17900af1b1564df38a71c05ec9b6a3645c23
frtim <frtim@ethz.ch>
2020-01-30 15:08:01 -0500

    Code ran for 512x512x512

--- param.py
+++ param.py
@@ -8,7 +8,7 @@ prefix = "Zebrafinch"
 
 data_path = "/n/pfister_lab2/Lab/tfranzmeyer/Data/512x512x512/1_discarded_512x512x512/"
 folder_path = "/n/pfister_lab2/Lab/tfranzmeyer/Data/512x512x512/holefilling/"
-code_path = "/n/pfister_lab2/Lab/tfranzmeyer/Data/512x512x512/Code/"
+code_path = "/n/pfister_lab2/Lab/tfranzmeyer/Data/512x512x512/Code/connected-components-3d/"
 output_path_filled_segments = "/n/pfister_lab2/Lab/tfranzmeyer/Data/512x512x512/2_discarded_filled_padded_512x512x512/"
 output_path_neuron_surfaces = "/n/pfister_lab2/Lab/tfranzmeyer/Data/512x512x512/d_seg_discarded_filled_surfaces/"
 
@@ -59,8 +59,8 @@ memory_step01_number = int(1.1*max_bs_z*max_bs_y*max_bs_x*(8+8+8)/1000/1000)
 memory_step00 = str(memory_step01_number)
 memory_step01 = str(memory_step01_number)
 memory_step02A = str(int(memory_step01_number*0.05))
-memory_step02B = str(int(memory_step01_number*0.5))
-memory_step03 = str(int(memory_step01_number*1))
+memory_step02B = str(int(memory_step01_number))
+memory_step03 = str(int(memory_step01_number*2))
 
 run_hours = str(4)
 
